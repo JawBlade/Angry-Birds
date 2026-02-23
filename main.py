b709ccfb9a20a2434427abbf21332c269a4ba0d5
JawBlade <122082998+JawBlade@users.noreply.github.com>
2026-02-23 07:37:39 -0800

    update

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from objects import Box
 import math
 
 
-height, width = (1280, 720)
+width, height = (1280, 720)
 pygame.init()
-screen = pygame.display.set_mode((height, width))
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
 
@@ -17,14 +17,22 @@ space = pymunk.Space()
 space.gravity = (0,900)
 draw_options = pymunk.pygame_util.DrawOptions(screen)
 
-box_1 = Box(100,100)
-
-body, box = box_1
-
+bird = Box((100, 100), (1280 // 2, 720 // 2), "brown")
+body, box = bird.create()
 space.add(body, box)
 
+bbox = Box((50, 100), (900, 720 // 2), "brown")
+body, box = bbox.create()
+space.add(body, box)
+
+floor_body = space.static_body
+floor_body.position = (0, 567)
+floor = pymunk.Segment(floor_body, (0, 0), (1280, 0), 1)
+floor.color = (103, 177, 20, 0)
+space.add(floor)
+
 background_image = pygame.image.load(os.path.join('images/back.jpg')).convert()
-background_image = pygame.transform.scale(background_image, (height, width))
+background_image = pygame.transform.scale(background_image, (width, height))
 
 while running:
 
@@ -36,11 +44,12 @@ while running:
         screen.blit(background_image, (0,0))
     else:
         screen.fill("black")
-
+    
     space.debug_draw(draw_options) # Draws Box on every frame
 
     pygame.display.flip()
 
     clock.tick(60)
+    space.step(1.0 / 60.0)
 
 pygame.quit()
--- objects.py
+++ objects.py
@@ -1,16 +1,34 @@
 import pymunk
 
+colors = {
+            'red':    (255, 0, 0, 0),
+            'orange': (255, 165, 0, 0),
+            'yellow': (255, 255, 0, 0),
+            'green':  (0, 255, 0, 0),
+            'blue':   (0, 0, 255, 0),
+            'indigo': (75, 0, 130, 0),
+            'purple': (128, 0, 128, 0),
+            'brown':  (223, 153, 35, 0)
+        }
+
 class Box:
-    def __init__(self, height: int, width :int):
-        self.height = height
-        self.width = width
+    def __init__(self, size: tuple, pos: tuple, color : str):
+        self.size = size
+        self.pos = pos
+        self.color = color.lower()
+
+        if self.color not in colors:
+            raise ValueError(f"'{self.color}' is invalid. Use ROYGBIV colors or Brown.")
 
     def create(self):
         # Make the rigid Body
-        body = pymunk.Body(mass=1, moment=10)
-        body.position = (1280 // 2, 720 // 2)
+        body = pymunk.Body(mass=0.2, moment=10)
+        body.position = self.pos
 
         # Create a Box
-        box = pymunk.Poly.create_box(body, (self.height, self.width))
+        box = pymunk.Poly.create_box(body, self.size)
+
+        box.color = colors[self.color] 
+        
 
         return body, box
