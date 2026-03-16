c1ee1b099c1524c50136f0fdacd828ba3349dd36
JawBlade <122082998+JawBlade@users.noreply.github.com>
2026-03-16 08:34:09 -0700

    FINALLLLY got the snapping bug fixed. So snapping works. Now Ill work on grabbing the bird and then launching.

--- main.py
+++ main.py
@@ -62,61 +62,49 @@ released=False
 
 while running:
     red.body_type = pymunk.Body.STATIC
-    x, y = red.position
-
-    # Calculate where to place the end of the band on the bird
-    dx = x - 225 
-    dy = y - 410 
-    angle_to_bird = math.atan2(dy, dx)
-    front_x = x + math.cos(angle_to_bird) * 36
-    front_y = y + math.sin(angle_to_bird) * 36
-    attach_point = (front_x, front_y) 
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
         if event.type == pygame.MOUSEBUTTONUP: 
             released = True
 
-
     button = pygame.mouse.get_pressed()
-
     if button[0]:     
         red.body_type = pymunk.Body.DYNAMIC
         red.position = pygame.mouse.get_pos()
         red.velocity = (0, 0)
         red.angular_velocity = 0
         red.angle = 0
+        released = False 
+
+    released = snap_check(red, released) 
+    space.step(1.0 / 60.0)
 
     screen.blit(image('images/back.jpg', (width, height)), (0,0))
-
-    space.debug_draw(draw_options)
+    
+    x, y = red.position
+    dx, dy = x - 225, y - 410 
+    angle_to_bird = math.atan2(dy, dx)
+    attach_point = (x + math.cos(angle_to_bird) * 36, y + math.sin(angle_to_bird) * 36)
 
     if button[0]:
         create_band(screen, band, (257, 413), attach_point)
 
     screen.blit(image('images/slingshot/right_stick_sling.png', (300,300)), (75,320))
-    
-    red_body.mask(screen, red)
+    red_body.mask(screen, red) 
 
     if button[0]:
         create_band(screen, band, (197, 418), attach_point)
 
     screen.blit(image('images/slingshot/left_stick_sling.png', (300,300)), (75, 320))
-    
     pig_b.mask(screen, pig)
 
     for body, box in boxes:
         body.mask(screen, box)
 
-    snap_check(red, released)
-    space.step(1.0 / 60.0)
-
     pygame.display.flip()
     clock.tick(60)
-
-pygame.quit()
 
 # where I got the Bird images
 # https://angrybirdsfanon.fandom.com/wiki/Angry_Birds_Chrome/Classic_artwork/sprites_Collection#Purple_Bird
